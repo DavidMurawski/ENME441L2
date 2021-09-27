508775e408a79db11ff4c81af09292d5e4abf132
David Murawski <91499129+DavidMurawski@users.noreply.github.com>
2021-09-27 20:10:44 +0000

    full code

--- lab2.py
+++ lab2.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setmode(GPIO.BOARD)
@@ -12,11 +13,37 @@ GPIO.setup(out1, GPIO.OUT, initial=0)
 GPIO.setup(out2, GPIO.OUT, initial=0)
 GPIO.setup(out3, GPIO.OUT, initial=0)
 
+dc1 = 50
+f1 = 1
+pwm0 = GPIO.PWM(out1, f1)
+pwm1 = GPIO.PWM(out2, f1)
+pwm2 = GPIO.PWM(out3, f1)
+
+pwm0.start(dc1)
 
 def myCallback(pin):
     print("Rising edge detected on pin %d" % pin)
+    if pin == in1:
+      try:
+        pwm1.start(dc1)
+        while True:
+          pass
+      except KeyboardInterrupt:
+        print('\nExiting')
+    if pin == in2:
+      try:
+        pwm2.start(dc1)
+        while True:
+          pass
+      except KeyboardInterrupt:
+        print('\nExiting')        
 
 GPIO.add_event_detect(in1, GPIO.RISING, callback=myCallback, bouncetime=100)
 
+GPIO.add_event_detect(in2, GPIO.RISING, callback=myCallback, bouncetime=100)
+
+while True:
+  print('.', end='')
+  time.sleep(0.1)
 
 GPIO.cleanup()
