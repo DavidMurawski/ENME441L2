c34be6eb930e39c958e4ba70a68a03fd2140d05a
David Murawski <91499129+DavidMurawski@users.noreply.github.com>
2021-09-28 01:38:41 +0000

    g

--- lab2.py
+++ lab2.py
@@ -13,7 +13,7 @@ GPIO.setup(out2, GPIO.OUT, initial=0)
 GPIO.setup(out3, GPIO.OUT, initial=0)
 
 dc1 = 50
-f1 = 1000
+f1 = 10000
 pwm0 = GPIO.PWM(out1, 1)
 pwm1 = GPIO.PWM(out2, f1)
 pwm2 = GPIO.PWM(out3, f1)
@@ -32,6 +32,7 @@ def myCallback(pin):
         for dc in range(0,100,1):
           pwm1.ChangeDutyCycle(dc)
           time.sleep(0.01)
+        pwm1.ChangeDutyCycle(100)
       if GPIO.input(in2) == GPIO.HIGH:
         pwm2.start(100)
         for dc in range(100,0,-1):
@@ -40,6 +41,7 @@ def myCallback(pin):
         for dc in range(0,100,1):
           pwm2.ChangeDutyCycle(dc)
           time.sleep(0.01)
+        pwm2.ChangeDutyCycle(100)
     except KeyboardInterrupt:
       print('\nExiting')
       GPIO.cleanup()      
